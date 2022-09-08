a98d5801c1d73e1d37df20878d45843c935ef004
Asabuoduinnocent <asabuoduinnocent@gmail.com>
2022-09-08 16:24:34 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Include Lightbox 
+import PhotoSwipeLightbox from '../photoswipe/dist/photoswipe-lightbox.esm.js';
+
+const lightbox = new PhotoSwipeLightbox({
+    // may select multiple "galleries"
+    gallery: '#gallery--getting-started',
+
+    // Elements within gallery (slides)
+    children: 'a',
+
+    // setup PhotoSwipe Core dynamic import
+    pswpModule: () =>
+        import ('../photoswipe/dist/photoswipe.esm.js')
+});
+lightbox.init();
